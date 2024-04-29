92fac811dda7a20038a3e297f84fe047cf45224a
Samuele-Benato <samuele.benato@gmail.com>
2024-04-29 14:42:47 +0200

    feat : add Address class

--- csharp-lista-indirizzi/Address.cs
+++ csharp-lista-indirizzi/Address.cs
@@ -8,5 +8,24 @@ namespace csharp_lista_indirizzi
 {
     public class Address
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public int ZIP { get; set; }
+
+        public Address(string name, string surname, string street, string city, string province, int zip)
+        {
+            Name = name;
+            Surname = surname;
+            Street = street;
+            City = city;
+            Province = province;
+            ZIP= zip;
+        }
+
+        public override string ToString() => $"{Name}, {Surname}, {Street}, {City}, {Province}, {ZIP}";
+        
     }
 }
