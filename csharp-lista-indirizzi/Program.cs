c09a66adb3b75fb2cdc6cf136cd810fcfa2e892d
Samuele-Benato <samuele.benato@gmail.com>
2024-04-29 14:10:25 +0200

    Aggiungere i file di progetto.

---
+++ csharp-lista-indirizzi/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace csharp_lista_indirizzi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Oggi esercitazione sui file, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe,
+            in particolare nel live-coding di oggi.
+            In questo esercizio dovrete leggere un file CSV,
+            che cambia poco da quanto appena visto nel live-coding in classe,
+            e salvare tutti gli indirizzi in esso contenuti all’interno di una lista di oggetti
+            istanziati a partire dalla classe Indirizzo.
+            Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari”
+            che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+            Buon Lavoro 
+            */
+        }
+    }
+}
